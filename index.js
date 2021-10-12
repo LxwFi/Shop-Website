6a1865891690da7fc4e35c44ad89328859b6d4ca
LxwFi <lxwfi404@gmail.com>
2021-10-12 10:39:13 +0100

    added new web methods + comments

--- index.js
+++ index.js
@@ -16,23 +16,23 @@ const item = new Items(db);
 const cat = new Categories(db);
 const cart = new Basket(db);
 web.use(express.static(path.join(__dirname, '/views')));
-
 const {
     allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
 const handlebars = expressHandlebars({
     handlebars: allowInsecurePrototypeAccess(Handlebars),
 });
-
 web.engine("handlebars", handlebars);
 web.set("view engine", "handlebars");
 web.set('views', path.join(__dirname, 'views'));
 web.use(express.urlencoded({ extended: true }));
 web.use(express.json());
 
+
+//main page + console log to show if someone is visiting
 web.get("/", async (req, res) => {
     const items = await item.all();
-    console.log(items);
+    console.log("Someone is visiting");
     res.render("home", { items });
 })
 
@@ -41,6 +41,8 @@ web.get("/create", (req, res) => {
 
 });
 
+
+//add new items, creates a new category if it doesnt exist
 web.post("/items", (req, res) => {
     const { title, price, description, category, image } = req.body
     if (!title || !price || !description || !category || !image) {
@@ -50,10 +52,13 @@ web.post("/items", (req, res) => {
     res.sendStatus(200);
 });
 
+
+//change the description of an item
 web.patch("/desc/:id", (req, res) => {
     if (req.params.id) {
-        item.descChange(req.params.id);
-        console.log("Deleting from cart with id " + req.params.id);
+        const {desc} = req.body
+        item.descChange(req.params.id, desc);
+        console.log("Changing desc for item with ID " + req.params.id);
         res.sendStatus(200);
         return;
     }
@@ -61,7 +66,19 @@ web.patch("/desc/:id", (req, res) => {
 });
 
 
-web.delete("/delete/:id", (req, res) => {
+//add to cart
+web.post("/cart/:id", (req, res) =>{
+    if (req.params.id) {
+        cart.add(req.params.id);
+        console.log("Adding item to cart with ID " + req.params.id);
+        res.sendStatus(200);
+        return;
+    }
+    res.sendStatus(400);
+});
+
+//remove from cart
+web.delete("/cart/:id", (req, res) => {
     if (req.params.id) {
         cart.remove(req.params.id);
         console.log("Deleting from cart with id " + req.params.id);
@@ -69,9 +86,10 @@ web.delete("/delete/:id", (req, res) => {
         return;
     }
     res.sendStatus(400);
-
 });
 
+
+//delete item 
 web.delete("/item/:id", (req, res) => {
     if (req.params.id) {
         item.remove(req.params.id);
@@ -80,12 +98,12 @@ web.delete("/item/:id", (req, res) => {
         return;
     }
     res.sendStatus(400);
-
 });
 
+//delete category (deletes all items in a category)
 web.delete("/category/:id", (req, res) => {
     if (req.params.id) {
-        category.remove(req.params.id);
+        cat.remove(req.params.id);
         console.log("Deleting category with id " + req.params.id);
         res.sendStatus(200);
         return;
@@ -93,6 +111,7 @@ web.delete("/category/:id", (req, res) => {
     res.sendStatus(400);
 });
 
+//function to create a database from seeded data
 async function runThis() {
     for (i of test) {
         const { title, price, description, category, image } = i
